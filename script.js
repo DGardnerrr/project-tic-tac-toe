a1a99c12122e54a2adc84efa3eb188104da54439
Deanna Gardner <dgardner3695@gmail.com>
2021-11-29 19:13:00 -0500

    update on script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//create variables
+//part - create variables
 //purpose: create variables for players so computer can differentiate between the 2 players
 //purpose: variable "winOutcomes" used to store possible winning outcomes 
 const player1 = 'x';
@@ -14,17 +14,26 @@ const winOutcomes = [
 	[2, 4, 6]
 ]
 
+//part 
 //set up dom nodes
 //purpose of this: use id tags from index html & save values of all game elements
-const cellElements = document.querySelectorAll('[data-cell]')
-const boardElement = document.getElementById('grid')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.getElementById('winningMessageText')
+//set player equal to false
 
-let player2 = false
 
-//functions & event listeners
-//purpose: set a call of function to begin the game & add event listeners 
-//make interactive for each button
-//created function for starting game, everytime a player wants to  restart the game, the game will restart
+
+
+//part - created function & event listeners for clicking on each cell/square
+
+
+
+
+//part - functions & event listeners
+//purpose: set a call of function to begin the game & add event listeners make interactive for each button
+//created function called "beginGame" for starting game, everytime a player wants to  restart the game, the game will restart
+
+
+	
+	
+//part -
+//function shows who won the game or if there is a tie - shows the 
+// results from  the game 
