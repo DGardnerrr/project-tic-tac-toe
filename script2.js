a1f909f9d7887aa521467a4b7820ed70f55d6599
Deanna Gardner <dgardner3695@gmail.com>
2021-12-01 00:16:40 -0500

    updated begin button, html & css

--- script2.js
+++ script2.js
@@ -27,31 +27,32 @@ const winningMessageElement = document.getElementById(".winMessage")
 const startButton = document.getElementById(".beginGame")
 const winningMessageTextElement = document.getElementById(".winningMessageText")
 
-
+//step 1 
 //function that allows player to start game 
 function startGame(name) {
     console.log("Hello you started the game" + name);
 };
-
 //added event listener for "begin game button"
-const beginGame = document.getElementById(".beginGame")
-beginGame.addEventListener("click", event => {
-  alert("The game has started!")
-})
+//const beginGame = document.getElementById(".beginGame")
 
+function handleClick() {
+	alert("The game has started!");
+}
 
 //when player clicks on the cell, either a X or O should pop up
-//add target to button
-//for loop - loop over all of data cells - 
-//add event listeners, so when you click a box
 //event listener for box
-const alertCell = document.getElementsByClassName(".box cell")
-alertCell.addEventListener('click', event => {
-   alert("You clicked a button!")
-})
-for (let i = 0; i < alertCell.length; i++) {
-console.log(i);
-}
+//step 1- add target to button
+//step 2 -for loop - loop over all of cells 
+//add event listeners, so when you click a box
+
+
+ const alertCell = document.getElementById(".box cell")
+ alertCell.addEventListener('click', event => {
+    alert("You clicked a button!")
+ })
+ for (let i = 0; i < alertCell.length; i++) {
+ console.log(i);
+ }
 
 // switch turns between 2 players
 
@@ -61,6 +62,6 @@ console.log(i);
 // code determines who won game
 // Create a function that checks if either player has won
 
-function pickWinner() {
-log("")
-}
+// function pickWinner() {
+// log("")
+// }
