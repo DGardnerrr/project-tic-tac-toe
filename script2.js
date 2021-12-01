c8b16d4bbec54c8aa69b0dafa14ca64669a0a358
Deanna Gardner <dgardner3695@gmail.com>
2021-12-01 13:39:32 -0500

    commit

--- script2.js
+++ script2.js
@@ -1,30 +1,59 @@
-// //part - create variables to store information 
-// //for clarification: whoever choose to play as X, the computer will generate that person as player A
-// //for clarification: whoever choose to play as cirle, the computer will generate that person as player B
-// //purpose: create variables for players so computer can differentiate between the 2 players
-// //purpose: variable "winOutcomes" used to store possible winning outcomes 
-// //variable "winOutcomes" will make it more simple for computer to determine who  will  win
-const x = 'X';
-const o = 'O';
+//set up dom nodes
+//first variable - target begin game button
+//second variable - target all of the boxes within columns
+const beginGame = document.getElementById(".beginGame")
+const boxes = document.querySelectorAll(".box")
 
-let currentPlayer = 'o';
+// part - create variables to store information 
+// for clarification: whoever choose to play as X, the computer will generate that person as player A
+//for clarification: whoever choose to play as cirle, the computer will generate that person as player B
+//purpose: create variables for players so computer can differentiate between the 2 players
+//purpose: variable "winOutcomes" used to store possible winning outcomes 
+//variable "winOutcomes" will make it more simple for computer to determine who  will  win
+const playerX = 'X';
+const playerO = 'O';
+const winOutcomes = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+] 
 
+//player who chooses O will go first
+//set currentPlayer variable equal to O
+let currentPlayer = O;
+
+//created function expression
+//created event listener for clicking on each box
+const boxClicked = (event) => {
+	const id = event.target.id
+
+	event.target.innerText = currentPlayer; //when click on each cell/box, it allows text to show up
+
+	boxes.forEach((box) => box.addEventListener("click", boxClicked))
+
+
+//created variable called board - serve as representation of tic tac toe game
+//
 const board = new Array(9).fill(null);
 
-const boxClicked = (e) => {
-	const id = e.target.id
-}
-
+//
 if (!board[id]) {
 	board[id] = currentPlayer;
-	e.target.innerText = currentPlayer;
-    if (checkline(()) endGame());
-	if(!board.some(e) => e === null)) endGame("draw"); //checks whether we have any null values
-
-currentPlayer = currentPlayer === O ? X : O
-}
-
-console.log(board);
+	event.target.innerText = currentPlayer;
+    if (checkline(()) )endGame());    //error fixed
+	if(!board.some(event) => event === null)) endGame("draw"); //checks whether we have any null values
+	console.log(board);
+//
+if (currentPlayer === O)
+currentPlayer = X
+else{
+currentPlayer = O
+	}
 };
 
 //determines who won the game 
@@ -38,7 +67,9 @@ boxes.forEach((box) => box.removeEventListener("click", boxClicked))
 const restartGame = () => {
 	currentPlayer = O;
 	board.fill(null)
-	
+	boxes.forEach((box) => {
+    box.innerText = "";
+	});
 
 const checkline = () => {
 // //winning combination - possible ways player  can win 
@@ -86,8 +117,6 @@ if(currentPlayer == board[0] && board[0] == board[1] && board[0] ==board[2])
 		return false;
 };
 
-boxes.forEach(box) => box.addEventListener("click", boxClicked));
-restart.addEventListener("click", restartGame)
 
 
 
