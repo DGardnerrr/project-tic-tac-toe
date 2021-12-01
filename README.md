3eda94a73cc40fb8951aab5cc9e045b05674dbcb
Deanna Gardner <dgardner3695@gmail.com>
2021-12-01 16:58:14 -0500

    commit

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 # project-tic-tac-toe
-
 Rules:
-one player is playing with "X", the other player is playing with "O"
+1. one player is playing with "X", the other player is playing with "O"
 
-If one player who owns X, gets three "X" in row, then that player wins
+2. If one player who owns X, gets three "X" in row, then that player wins
 
-If the other player who owns O, gets three "O" in a row, then that player wins 
+3. If the other player who owns O, gets three "O" in a row, then that player wins 
 
-If both players are not able to get their "X" or "O" in a row, then they both lose
+4. If both players are not able to get their "X" or "O" in a row, then they both lose
 
 
-
-user stories:
-
+User stories:
 1. As a player, I can play a game of tic tac toe with the other player
 
-2. As a player, I should be able to start the game by clicking begin  game
+2. As a player, I should be able to reset game by clicking reset game
 
 3. Switching turns between X and O for player A: 
 In order to give the player 1 a challenge, player 2 will need to choose a move to play
@@ -25,6 +22,33 @@ In order to give the player 2 a challenge, player 1 will need to choose a move t
 
 
 5. As a player uncertain of the rules I want to be told who won and why so that I know whether I was successful in the game 
+
+
+Main Takeaways From This Project:
+1. Time Management, Organization, Consistent - being able to effectively make a plan on how your going to effectively work on the index.html, css, and javascript
+
+
+2. Learning about the importance of concepts such as innerHTML, DOM selection, etc - these concepts play a major role in tic tac toe game - user interactions
+
+3. Being able to incorporate some of the concepts that were taught in the previous classes so far to this project
+
+
+Additional steps I would have Took/Implementations:
+1. Add more bonuses to my project 
+- spending more time on my HTML - including a little more content to make game more interactive 
+
+- being more creative with user face interactions with the tic tac toe game: including 
+
+
+Some of Issues Encountered:
+1. The use of DOM - not really familiar with DOM
+
+
+
+
+
+
+
 
 
 
--- script2.js
+++ script2.js
@@ -1,17 +1,23 @@
 //set up dom nodes
-//first variable - target begin game button
-//second variable - target all of the boxes within columns
-const beginGame = document.getElementById(".beginGame")
-const boxes = document.querySelectorAll(".box")
+//first variable - target reset game button, added event listeners
+//second variable - target all of the boxes within columns, added event listeners
+const resetGame = document.getElementById(".resetGame")
+const reset = document.getElementby(".")
+
+const boxes = document.querySelectorAll(".cell")
+const boxClick = (event) => {
+	console.log(event.target.id);
+};
+boxes.forEach((cell) => cell.addEventListener("click", boxClicked)) 
 
 // part - create variables to store information 
-// for clarification: whoever choose to play as X, the computer will generate that person as player A
-//for clarification: whoever choose to play as cirle, the computer will generate that person as player B
+// for clarification: whoever choose to play as O, the computer will generate that person as player 1
+//for clarification: whoever choose to play as X, the computer will generate that person as player 2
 //purpose: create variables for players so computer can differentiate between the 2 players
 //purpose: variable "winOutcomes" used to store possible winning outcomes 
 //variable "winOutcomes" will make it more simple for computer to determine who  will  win
-const playerX = 'X';
-const playerO = 'O';
+const signX = 'X';
+const signO = 'O';
 const winOutcomes = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -24,7 +30,7 @@ const winOutcomes = [
 ] 
 
 //player who chooses O will go first
-//set currentPlayer variable equal to O
+//set currentPlayer variable equal to the letter 0
 let currentPlayer = O;
 
 //created function expression
@@ -32,38 +38,51 @@ let currentPlayer = O;
 const boxClicked = (event) => {
 	const id = event.target.id
 
-	event.target.innerText = currentPlayer; //when click on each cell/box, it allows text to show up
+	event.target.innerText = currentPlayer; //when click on each cell/box, it allows text to show up (HTML)
 
-	boxes.forEach((box) => box.addEventListener("click", boxClicked))
-
+//this code enables game to switch between players
+//created the if condition
+//assigned currentPlayer to the if condition
+currentPlayer = (currentPlayer === O) ? X : O	
+  
+// //This is also Equivalent to:
+// if (currentPlayer === O)
+// 	currentPlayer = X 
+// 	else {
+// 		currentPlayer = O
+// 	};
 
 //created variable called board - serve as representation of tic tac toe game
-//
+//created a new array of 9 boxes and fill/reserve each space with null
 const board = new Array(9).fill(null);
 
-//
+//if statement that checks whether the board is null
+//This helps to keep track of our game
+//if checkline returns true, then the game ends
 if (!board[id]) {
 	board[id] = currentPlayer;
 	event.target.innerText = currentPlayer;
-    if (checkline(()) )endGame());    //error fixed
-	if(!board.some(event) => event === null)) endGame("draw"); //checks whether we have any null values
-	console.log(board);
-//
-if (currentPlayer === O)
-currentPlayer = X
-else{
-currentPlayer = O
+    if (checkline()) endGame();
+	if(!board.some(event) => )
 	}
-};
+	console.log(board);
 
-//determines who won the game 
+//Equivalent to This:
+// if (currentPlayer === O)
+// currentPlayer = X
+// else{
+// currentPlayer = O
+// 	}
+// };
+
+//function determines the results of the end game 
 const endGame = (result) => {
 intro.innerText = result == "draw" ? "Draw!" : currentPlayer + "has won!";
 boxes.forEach((box) => box.removeEventListener("click", boxClicked))
 	console.log("game ended");
 }
 
-
+//function allows player to restart the game 
 const restartGame = () => {
 	currentPlayer = O;
 	board.fill(null)
@@ -84,7 +103,8 @@ const checkline = () => {
 // ]
 
 
-//check if board[0] is same as board[1]
+//
+//for instance, if statement checks if board[0] is same as board[1]
 
 //Top Line = [0,1,2] X 
 if(currentPlayer == board[0] && board[0] == board[1] && board[0] ==board[2]) 
@@ -116,17 +136,4 @@ if(currentPlayer == board[0] && board[0] == board[1] && board[0] ==board[2])
 //If none of  these conditions are met, it will return  false
 		return false;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
