583e959d5aed5caf7122ad259cc87ac37653434c
Deanna Gardner <dgardner3695@gmail.com>
2021-12-05 00:57:12 -0500

    revised better game logic

--- script3.js
+++ script3.js
@@ -1,15 +1,25 @@
 //DOM selector 
-
+const boxes = document.getElementsByClassName('box')
 
 // step - create variables to store information for game
-
-
-
-
+let X_or_O = 0
 
 
 //step - create "for loop" for when each player with X or O click on each box 
-
+for(let i = 0; i < boxes.length; i++) {
+    boxes[i].onclick = function() {
+        if(X_or_O % 2 === 0) {
+            this.innerHTML='X'
+            winner()
+            X_or_O+=1
+        }
+        else {
+            this.innerHTML='O'
+            winner()
+            X_or_O+=1
+        }
+    }
+}
 
 
 
